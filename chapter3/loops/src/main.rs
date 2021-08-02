898e62e11e6dffc9018e0399a8ec7ed2abe02954
sod-lol <ddssee5511@gmail.com>
2021-08-02 03:42:58 -0400

    loop with break added

--- chapter3/loops/src/main.rs
+++ chapter3/loops/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
     
+    let mut g = 0;
 
-    loop {
-        println!("fuck  you");
-    }
+    let resutl = loop {
+        g += 1;
+
+        if g >= 10 {
+            break g * 4;
+        }
+
+        println!("fuck youuuuuuuuuuuuu");
+    };
+
+    println!("after loop got {}", resutl);
 }
