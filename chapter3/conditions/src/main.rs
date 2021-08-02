1b19f174e978761fd27510b7972355bd3586cdb7
sod-lol <ddssee5511@gmail.com>
2021-08-02 03:35:08 -0400

    learned if else if

--- chapter3/conditions/src/main.rs
+++ chapter3/conditions/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
     
-    let con = 5;
+    let g: i64 = 493847457938;
 
-    if con < 3 {
-        println!("hub!");
-    } else {
-        println!("hub1!");
+    if g % 7 == 0 {
+        println!("its 0");
+    } else if g % 7 == 1 {
+        println!("its 1");
+    } else if g % 7 == 2 {
+        println!("its 2");
+    } else if g % 7 == 3 {
+        println!("its 3");
+    } else if g % 7 == 4 {
+        println!("its 4");
+    } else if g % 7 == 5 {
+        println!("its 5");
+    } else if g % 7 == 6 {
+        println!("its 6");
     }
 }
