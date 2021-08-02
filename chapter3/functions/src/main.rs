56c69b90d51966bfdbf1cf03c99f5bfe66db0a59
sod-lol <ddssee5511@gmail.com>
2021-08-02 02:22:45 -0400

    expression based value added

--- chapter3/functions/src/main.rs
+++ chapter3/functions/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(5);
+    let y = {
+        let mut x = 4;
+        x *= 4;
+        x + 1
+    };
+
+    println!("Another function. {}", y);
+
+    another_function(3);
 }
 
 fn another_function(x: i32) {
