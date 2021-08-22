a871d500e67a52fbce3b42bbf3756f827bbbfdc1
sod-lol <ddssee5511@gmail.com>
2021-08-22 08:33:58 -0400

    basic-strcut usage added

---
+++ chapter5/basic-struct/src/main.rs
@@ -0,0 +1,22 @@
+
+
+
+struct User {
+    user: String,
+    email: String, 
+    is_active: bool,
+    count_of_sign: u64,
+}
+
+fn main() {
+
+
+    let u1 = User {
+        user: String::from("Hoho"),
+        email: String::from("Hoho@hoho.com"),
+        is_active: true,
+        count_of_sign: 1,
+    };
+
+    println!("User info is {} with email {}, and it's {}", u1.user, u1.email, u1.is_active)
+}
