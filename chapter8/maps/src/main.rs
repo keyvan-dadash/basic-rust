8ed225bcc036d2782caeac15f6095488102479ff
sod-lol <ddssee5511@gmail.com>
2021-08-23 23:51:57 -0400

    update value with entry method added

--- chapter8/maps/src/main.rs
+++ chapter8/maps/src/main.rs
@@ -33,4 +33,17 @@ fn main() {
     for (keyy, valuee) in &map {
         println!("value {} is for key {}", valuee, keyy);
     }
+
+    //overwrite value
+    map.insert(String::from("Key"), 10);
+
+    map.entry("Key2".to_string()).or_insert(20);
+    map.entry("Key".to_string()).or_insert(20);
+
+    println!("map is {:#?}", map);
+
+    let number = map.entry("Key".to_string()).or_insert(20);
+    *number *= 5;
+
+    println!("map is {:#?}", map);
 }
