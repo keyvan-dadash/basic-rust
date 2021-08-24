eeab3929af21ce8360063323d05e6afc877e55b2
sod-lol <ddssee5511@gmail.com>
2021-08-23 23:42:17 -0400

    basic map usage learned

---
+++ chapter8/maps/src/main.rs
@@ -0,0 +1,12 @@
+
+
+use std::collections::HashMap;
+
+fn main() {
+    
+    let mut map = HashMap::new();
+
+    map.insert(String::from("Key"), 5);
+    map.insert(String::from("Key1"), 10);
+
+}
