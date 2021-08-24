bef64cf17f2795a27792ae9494edbc9acf50a810
sod-lol <ddssee5511@gmail.com>
2021-08-23 23:45:23 -0400

    get value by key with match pattern learned

--- chapter8/maps/src/main.rs
+++ chapter8/maps/src/main.rs
@@ -9,4 +9,24 @@ fn main() {
     map.insert(String::from("Key"), 5);
     map.insert(String::from("Key1"), 10);
 
+    let key = String::from("Key");
+
+    match map.get(&key) {
+        Some(value) => {
+            println!("value for key is {}", value);
+        },
+        None => {
+            println!("value does not exists");
+        }
+    }
+
+    let invalid_key = String::from("sdlkjf");
+    match map.get(&invalid_key) {
+        Some(value) => {
+            println!("value for key is {}", value);
+        },
+        None => {
+            println!("value does not exists");
+        }
+    }
 }
