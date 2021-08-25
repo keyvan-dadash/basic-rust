1484174292e18ceb7f6d6e8ce2ed23d049ca8e67
sod-lol <ddssee5511@gmail.com>
2021-08-25 15:51:17 -0400

    new test added

--- chapter11/adder/src/lib.rs
+++ chapter11/adder/src/lib.rs
@@ -1,7 +1,39 @@
+
+
+
 #[cfg(test)]
 mod tests {
+
+    pub struct Rect {
+        width: u32,
+        height: u32,
+    }
+    
+    
+    impl Rect {
+    
+        pub fn can_hold(&self, other: &Rect) -> bool {
+            return (self.width > other.width) && (self.height > other.height)
+        }
+    }
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn rect_test() {
+        let rect1 = Rect {
+            width: 8,
+            height: 7,
+        };
+
+        let rect2 = Rect {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(rect1.can_hold(&rect2));
     }
 }
