a0fae555e19107ac198413f77ba747c246801c89
sod-lol <ddssee5511@gmail.com>
2021-08-26 08:09:20 -0400

    iterator adaptors method used

--- chapter13/iter/src/main.rs
+++ chapter13/iter/src/main.rs
@@ -7,8 +7,8 @@ fn main() {
     
     let v1 = vec![1, 2, 3];
 
-    let v1_iter = v1.iter();
+    let v1_iter: Vec<_> = v1.iter().map(|x| x + 1).collect();
 
-    let total: u32 = v1_iter.sum();
+    let total: u32 = v1_iter.iter().sum();
     println!("sum of vector is {}", total);
 }
