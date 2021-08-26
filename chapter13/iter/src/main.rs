a7e22cdb38ed7c38674674b0df7f9d539c16a920
sod-lol <ddssee5511@gmail.com>
2021-08-26 08:16:41 -0400

    iterator implemented for some struct

--- chapter13/iter/src/main.rs
+++ chapter13/iter/src/main.rs
@@ -1,14 +1,48 @@
 
 
 
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+
+    fn new() -> Counter {
+        return Counter {
+            count: 0,
+        }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> 
+    {
+        if self.count < 5 {
+            self.count += 1;
+            return Some(self.count);
+        } else {
+            return None;
+        }
+    }
+}
 
 
 fn main() {
     
-    let v1 = vec![1, 2, 3];
+    // let v1 = vec![1, 2, 3];
 
-    let v1_iter: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    // let v1_iter: Vec<_> = v1.iter().map(|x| x + 1).collect();
 
-    let total: u32 = v1_iter.iter().sum();
-    println!("sum of vector is {}", total);
+    // let total: u32 = v1_iter.iter().sum();
+    // println!("sum of vector is {}", total);
+
+    let mut counter = Counter::new();
+
+    println!("vlaue is {:#?}", counter.next());
+    println!("vlaue is {:#?}", counter.next());
+    println!("vlaue is {:#?}", counter.next());
+    println!("vlaue is {:#?}", counter.next());
+    println!("vlaue is {:#?}", counter.next());
 }
