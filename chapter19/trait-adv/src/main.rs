e03090e0803bfdd3971ad8fa46fd3b6b32f0259f
sod-lol <ddssee5511@gmail.com>
2021-12-27 20:02:38 -0500

    trait-adv updated

--- chapter19/trait-adv/src/main.rs
+++ chapter19/trait-adv/src/main.rs
@@ -7,10 +7,26 @@ struct Point {
     y: i32,
 }
 
+struct Point2 {
+    x: i32,
+    y: i32,
+}
+
 impl Add for Point {
     type Output = Point;
 
     fn add(self, other: Point) -> Point {
+        return Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+} 
+
+impl Add<Point2> for Point {
+    type Output = Point;
+
+    fn add(self, other: Point2) -> Point {
         return Point {
             x: self.x + other.x,
             y: self.y + other.y,
@@ -24,6 +40,11 @@ fn main() {
 
     assert_eq!(
         Point{ x: 1, y: 2 } + Point{ x: 2, y: 4 },
+        Point{ x: 3, y: 6 }
+    );
+
+    assert_eq!(
+        Point{ x: 1, y: 2 } + Point2{ x: 2, y: 4 },
         Point{ x: 3, y: 6 }
     );
 }
